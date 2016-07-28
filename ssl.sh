636f8a2e5d8694b69bb613dc3c5810c478159267
Aki Sasaki <aki@escapewindow.com>
2016-07-28 14:22:18 -0700

    attempt to create san cert, failing to verify

--- ssl.sh
+++ ssl.sh
@@ -1,33 +1,35 @@
 #!/bin/sh -ex
 mkdir -p ssl
-DOCKER_IP=127.0.0.1
 cd ssl
 if [ -f myssl.cnf ] ; then
     rm -f myssl.cnf
 fi
-for file in /usr/local/etc/openssl/openssl.cnf /etc/ssl/openssl.cnf ; do
-    if [ -f $file ] ; then
-        cp $file myssl.cnf
-        printf "[SAN]\nsubjectAltName=DNS:$DOCKER_IP" >> myssl.cnf
-        break
-    fi
-done
-if [ ! -f myssl.cnf ] ; then
-    echo "Can't find openssl.cnf!"
-    exit 1
-fi
-openssl req \
-    -new \
-    -newkey rsa:4096 \
-    -days 365 \
-    -nodes \
-    -x509 \
-    -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=$DOCKER_IP/subjectAltName=DNS:$DOCKER_IP" \
-    -reqexts SAN \
-    -config myssl.cnf \
-    -keyout docker.key \
-    -out docker.cert
-cat docker.key >> docker.cert
-cp docker.key ../host.key; cp docker.cert ../host.cert
+cp ../myssl.cnf.tmpl myssl.cnf
+printf "DNS.1 = 127.0.0.1\nDNS.2 = localhost\nDNS.3 = localhost.localdomain" >> myssl.cnf
+#openssl req \
+#    -new \
+#    -newkey rsa:4096 \
+#    -days 730 \
+#    -nodes \
+#    -x509 \
+#    -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=localhost" \
+#    -config myssl.cnf \
+#    -keyout host.key \
+#    -out host.cert
 #openssl rsa -in privkey.pem -out key.pem; openssl x509 -in cert.csr -out cert.pem -req -signkey key.pem -days 1001; cat key.pem>>cert.pem
 #cp key.pem ../host.key; cp cert.pem ../host.cert
+#
+openssl genrsa -out host.key 3072
+openssl req -new \
+    -config myssl.cnf \
+    -x509 \
+    -key host.key \
+    -extensions ssl_client \
+    -sha256 \
+    -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=127.0.0.1" \
+    -out docker.cert \
+    -days 730
+
+cp host.cert docker.cert
+cat host.key >> host.cert
+cp host.key host.cert docker.cert ../
