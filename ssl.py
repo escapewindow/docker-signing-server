aa84e07c6e49a0f121d4c4eb31e45c8ecc18588b
Aki Sasaki <aki@escapewindow.com>
2016-07-30 17:30:32 -0700

    argparse and cleanup

--- ssl.py
+++ ssl.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
+"""Generate a new self-signed private key + public cert for an ssl server.
+
+This will contain a CommonName and subjectAltName for newer python ssl
+verification.
+
+This requires openssl installed locally.
+"""
 from __future__ import print_function
+import argparse
 import os
-import shutil
 import subprocess
 import sys
 
@@ -16,11 +23,7 @@ SSL_CONFIG_PATHS = (
     "/etc/ssl/openssl.cnf",
     "/usr/lib/ssl/openssl.cnf",
 )
-NEW_CONFIG_PATH = "myssl.cnf"
-SSL_DIR = os.path.join(os.abspath(os.dirname(__file__)), 'ssl')
-# DEFAULT_ALTNAME = "IP.1:127.0.0.1,IP.2:172.17.0.2,DNS.1:localhost,DNS.2:localhost.localdomain"
-DEFAULT_REPLACE = {"fqdn": "localhost"}
-DEFAULT_ALTNAME = "DNS.1:%(fqdn)s"
+SSL_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'ssl')
 DEFAULT_SUBJECT = "/C=US/ST=CA/L=San Francisco/O=Mozilla/CN=%(fqdn)s"
 
 
@@ -35,59 +38,109 @@ def generate_new_ssl_conf(orig_config_string):
     # stop lowercasing key names!!
     config.optionxform = lambda option: option
     # add [default] section at the top to keep configparser from barfing
-    config_string = "# Modified per http://stackoverflow.com/a/21494483\n[ default ]\n{}".format(orig_config_string)
+    config_string = "# Modified per http://stackoverflow.com/a/21494483\n" + \
+                    "[ default ]\n{}".format(orig_config_string)
     config.read_string(config_string)
+
     # print(config.sections())
-    config.set(' CA_default ','copy_extensions', r'copy')  # barfs without the spaces =\
+
+    # The section names barf without the spaces =\
+    config.set(' CA_default ', 'copy_extensions', r'copy')
     config.set(' v3_ca ', 'subjectAltName', r'$ENV::ALTNAME')
     return config
 
 
-def read_orig_ssl_conf(search_paths):
+def read_orig_ssl_conf(path, search_paths):
     """Find openssl.cnf and return its contents as a string
     """
+    if path is not None:
+        search_paths = [path]
     for path in search_paths:
         if os.path.exists(path):
             with open(path, "r") as fh:
                 return fh.read()
     else:
-        raise Exception("Can't find openssl.cnf in %s!"% search_paths)
+        raise Exception("Can't find openssl.cnf in %s!" % search_paths)
 
 
 def run_cmd(cmd):
+    """Run a command.
+    """
     print("Running %s..." % cmd)
     subprocess.check_call(cmd)
 
 
-def generate_keys():
-    run_cmd(["openssl", "genrsa", "-out", "ssl/host.key", "3072"])  # TODO hostname
+def build_altname(fqdns, ips):
+    altname = []
+    for num, val in enumerate(fqdns, start=1):
+        altname.append("DNS.%d:%s" % (num, val))
+    for num, val in enumerate(ips, start=1):
+        altname.append("IP.%d:%s" % (num, val))
+    return ','.join(altname)
+
+
+def generate_keys(options):
+    """Generate the key and cert.
+    """
+    hostname = options.dns[0]
+    key = os.path.join(SSL_DIR, "%s.key" % hostname)
+    cert = os.path.join(SSL_DIR, "%s.cert" % hostname)
+    os.environ['ALTNAME'] = build_altname(options.fqdn, options.ips)
+    print("Using ALTNAME of %s ..." % os.environ['ALTNAME'])
+    for path in (key, cert):
+        if os.path.exists(path):
+            os.remove(path)
+    repl_dict = {
+        'fqdn': hostname,
+    }
+    run_cmd(["openssl", "genrsa", "-out", key, str(options.days)])
     run_cmd([
         "openssl", "req",
         "-new",
         "-x509",
-        "-config", NEW_CONFIG_PATH,
-        "-key", "ssl/host.key",  # TODO hostname
+        "-config", options.newconf,
+        "-key", key,
         "-sha256",
-        "-subj", DEFAULT_SUBJECT,
-        "-out", "ssl/host.cert",  # TODO hostname
-        "-days", "730",
+        "-subj", options.subject % repl_dict,
+        "-out", cert,
+        "-days", str(options.days),
     ])
+    return key, cert
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        description='Generate self-signed SSL cert for a host.'
+    )
+    parser.add_argument('--fqdn', metavar='fqdn', type=str, nargs='+',
+                        required=True,
+                        help='FQDN(s) for the SSL server (first is primary)')
+    parser.add_argument('--ips', metavar='X.X.X.X', type=str, nargs='+',
+                        help='IP address(es) for the SSL server')
+    parser.add_argument('--days', type=int, default=365,
+                        help='Number of days before expiration')
+    parser.add_argument('--newconf', type=str, default="myssl.cnf",
+                        help='Path to write generated openssl config')
+    parser.add_argument('--openssl-path', type=str, help='Path to openssl.cnf')
+    parser.add_argument('--subject', type=str, default=DEFAULT_SUBJECT,
+                        help='openssl req subject')
+    return parser.parse_args(args)
 
 
 def main(name=None):
     if name not in (None, '__main__'):
         return
-    # TODO argparse
-    config = generate_new_ssl_conf(read_orig_ssl_conf(SSL_CONFIG_PATHS))
-    with open(NEW_CONFIG_PATH, 'w') as fh:
-        config.write(fh)
-
-    os.environ['ALTNAME'] = os.environ.get('ALTNAME', DEFAULT_ALTNAME % DEFAULT_REPLACE)
-    for path in ("host.key", "host.cert"):
-        if os.path.exists(path):
-            os.remove(path)
-
-    generate_keys()
+    options = parse_args(sys.argv[1:])
+    ssl_conf = generate_new_ssl_conf(
+        read_orig_ssl_conf(options.openssl_path, SSL_CONFIG_PATHS)
+    )
+    with open(options.newconf, 'w') as fh:
+        ssl_conf.write(fh)
+    key, cert = generate_keys()
+    print("Private key is at %s.  Public cert is at %s." % (key, cert))
+    print("You can inspect the cert via `openssl x509 -text -noout -in %s`"
+          % cert)
+    print("Done.")
 
 
 main(name=__name__)
