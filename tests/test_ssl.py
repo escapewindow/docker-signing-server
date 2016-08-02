b8502c1a55365b80b4e1035b9960068e4acefe7f
Aki Sasaki <aki@escapewindow.com>
2016-08-02 11:41:01 -0700

    30% coverage

--- tests/test_ssl.py
+++ tests/test_ssl.py
@@ -17,6 +17,7 @@ def fake_ssl_conf():
         return fh.read()
 
 
+# generate_new_ssl_conf {{{1
 def test_generate_new_ssl_conf(fake_ssl_conf):
     options = argparse.Namespace()
     options.ca_dir = "CA_DIR"
@@ -33,3 +34,14 @@ def test_generate_new_ssl_conf_ca(fake_ssl_conf):
     assert config.get(' default ', 'HOME') == '.'
     assert config.has_option(' v3_ca ', 'subjectAltName') is False
     assert config.get(' CA_default ', 'dir') == options.ca_dir
+
+
+# read_orig_ssl_conf {{{1
+def test_read_orig_ssl_conf(fake_ssl_conf):
+    val = csrtool.read_orig_ssl_conf(os.path.join(DATA_DIR, "ssl.cnf"), [])
+    assert val == fake_ssl_conf
+
+
+def test_missing_orig_ssl_conf(fake_ssl_conf):
+    with pytest.raises(Exception):
+        csrtool.read_orig_ssl_conf(None, ["x"])
