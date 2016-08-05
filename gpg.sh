c2e0b491637d4077fbef530c2a4eb3c71e8a7604
Aki Sasaki <aki@escapewindow.com>
2016-08-05 16:19:19 -0700

    move docker files into docker/
    instead of maintaining a large .dockerignore

--- gpg.sh
+++ gpg.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gpg2 --no-default-keyring --secret-keyring gpg/secring.gpg --keyring gpg/pubring.gpg "$@"
+gpg2 --no-default-keyring --secret-keyring docker/gpg/secring.gpg --keyring docker/gpg/pubring.gpg "$@"
--- ssl.sh
+++ ssl.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -ex
 rm -rf fake_ca
 ./csrtool.py gen_ca gen_csr sign_csr --ca-dir fake_ca --ca-pass --fqdn localhost localhost.localdomain --ip 127.0.0.1
-cp ssl/localhost.cert host.cert
-cp ssl/localhost.key host.key
+cp ssl/localhost.cert docker/host.cert
+cp ssl/localhost.key docker/host.key
 cat <<EOF
 You can now launch the docker signing server, and use fake_ca/ca.crt to verify the cert.
 
 To enhance security, run the following (but it will take a few minutes):
     ./csrtool.py ecdh_cert --fqdn localhost
-    cp ssl/localhost.cert host.cert
+    cp ssl/localhost.cert docker/host.cert
 EOF
